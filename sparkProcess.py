89d9d1022715396cca4ff434da38a3614c1d21de
yujin19 <sysujinyu@twitter.us-east1-b.c.aroundgo-236002.internal>
2019-05-03 04:34:05 +0000

    add visualize and fix the filter bug

--- sparkProcess.py
+++ sparkProcess.py
@@ -22,7 +22,7 @@ def return_sql_context_instance(spark_context):
 
 def stream_dataframe_to_flask(df):
     top_tags = [str(t.tag) for t in df.select("tag").collect()]
-    tags_count = [p.count for p in df.select("count").collect()]
+    tags_count = [p.counts for p in df.select("counts").collect()]
     url = 'http://0.0.0.0:5050/updateData'
     request_data = {'words': str(top_tags), 'counts': str(tags_count)}
     response = requests.post(url, data=request_data)
@@ -31,23 +31,21 @@ def process_rdd(time, rdd):
     print("------------- %s --------------" % str(time))
     try:
         sql_context_instance = return_sql_context_instance(rdd.context)
-        row_rdd = rdd.map(lambda w: Row(tag=w[0], count=w[1]))
+        row_rdd = rdd.map(lambda w: Row(tag=w[0], counts=w[1]))
         print(row_rdd)
         tags_counts_df = sql_context_instance.createDataFrame(row_rdd)
         tags_counts_df.registerTempTable("tag_with_counts")
-        selected_tags_counts_df = sql_context_instance.sql("select tag, count from tag_with_counts order by count desc limit 8")
+        selected_tags_counts_df = sql_context_instance.sql("select tag, counts from tag_with_counts order by counts desc limit 8")
         selected_tags_counts_df.show()
-        #stream_dataframe_to_flask(selected_tags_counts_df)
+        stream_dataframe_to_flask(selected_tags_counts_df)
     except:
         e = sys.exc_info()[0]
         print("Error: %s" % e)
 
-words = dataStream.flatMap(lambda line: line.split(" "))# words is array of string, string is words, not sentence
-#hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
-hashtags = words.filter(lambda w: '@' in w).map(lambda x: (x, 1)) # process email count in tweets
+words = dataStream.flatMap(lambda line: line.split(" "))
+hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
+# hashtags = words.filter(lambda w: '@' in w).map(lambda x: (x, 1)) # process email count in tweets
 tags_totals = hashtags.updateStateByKey(sumup_tags_counts)
 tags_totals.foreachRDD(process_rdd)
 ssc.start()
 ssc.awaitTermination()
-
-
